19d258897da4493a8d2676e486a663139e90edf4
Maoyeedy <100255436+Maoyeedy@users.noreply.github.com>
2023-06-09 15:17:41 +0800

    0.0

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,8 @@
 module.exports = {
     plugins: [
-        require('postcss-preset-env'),
         require('autoprefixer'),
         require('cssnano')({
-            preset: 'advanced',
+            preset: 'default',
         }),
     ],
 };
