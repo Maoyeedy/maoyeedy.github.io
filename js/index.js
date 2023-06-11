73aefd1781bbe5bf640b8a93252437dd4b42286c
Maoyeedy <100255436+Maoyeedy@users.noreply.github.com>
2023-06-12 04:38:57 +0800

    0.0

--- js/index.js
+++ js/index.js
@@ -1,62 +1,66 @@
-const main = document.getElementById("main"),
-    container = document.getElementById("container"),
-    maoyeedy = document.getElementById("name"),
-    redButton = document.getElementById("red-button-placeholder"),
-    leftPanel = document.getElementById("left"),
-    rightPanel = document.getElementById("right"),
-    leftTrigger = document.getElementById("left-trigger"),
-    rightTrigger = document.getElementById("right-trigger"),
-    circle1 = document.getElementById("circle1"),
-    circle2 = document.getElementById("circle2"),
-    scrollPercentage = (window.innerHeight / 100) * 46;
+const main = document.getElementById("main");
+const container = document.getElementById("container");
+const maoyeedy = document.getElementById("name");
+const redButton = document.getElementById("red-button-placeholder");
+const leftPanel = document.getElementById("left");
+const rightPanel = document.getElementById("right");
+const leftTrigger = document.getElementById("left-trigger");
+const rightTrigger = document.getElementById("right-trigger");
+const circle1 = document.getElementById("circle1");
+const circle2 = document.getElementById("circle2");
+const scrollPercentage = (window.innerHeight / 100) * 46;
 
-function setColor(e) {
-    (circle1.style.backgroundColor = e ? "#93969f" : "white"),
-        (circle2.style.backgroundColor = e ? "white" : "#93969f");
+function setColor(isDark) {
+    circle1.style.backgroundColor = isDark ? "#93969f" : "white";
+    circle2.style.backgroundColor = isDark ? "white" : "#93969f";
 }
-function scrollMain(e) {
-    (main.scrollTop += e ? scrollPercentage : -1 * scrollPercentage), setColor(e);
+
+function scrollMain(isDown) {
+    main.scrollTop += isDown ? scrollPercentage : -1 * scrollPercentage;
+    setColor(isDown);
 }
+
 function toggleLeftPanel() {
     container.classList.toggle("show-left-panel");
 }
+
 function toggleRightPanel() {
     container.classList.toggle("show-right-panel");
 }
+
 function loadDeferContent() {
-    document.querySelectorAll(".defer img").forEach(function (e) {
-        e.src = e.dataset.src;
+    const images = document.querySelectorAll(".defer img");
+    images.forEach((img) => {
+        img.src = img.dataset.src;
     });
 }
 
-circle1.addEventListener("click", function () {
-    scrollMain(!1);
-}),
-    circle2.addEventListener("click", function () {
-        scrollMain(!0);
-    }),
-    main.addEventListener(
-        "wheel",
-        function (e) {
-            setColor(0 < e.deltaY);
-        },
-        { passive: !0 }
-    ),
-    document.addEventListener("keydown", function (e) {
-        "ArrowDown" === e.key || "ArrowRight" === e.key ? scrollMain(!0) : ("ArrowUp" !== e.key && "ArrowLeft" !== e.key) || scrollMain(!1);
-    }),
-    maoyeedy.addEventListener("mouseover", toggleLeftPanel),
-    maoyeedy.addEventListener("mouseout", toggleLeftPanel),
-    redButton.addEventListener("mouseover", toggleRightPanel),
-    redButton.addEventListener("mouseout", toggleRightPanel),
-    leftPanel.addEventListener("mouseover", toggleLeftPanel),
-    leftPanel.addEventListener("mouseout", toggleLeftPanel),
-    rightPanel.addEventListener("mouseover", toggleRightPanel),
-    rightPanel.addEventListener("mouseout", toggleRightPanel),
-    leftTrigger.addEventListener("mouseover", toggleLeftPanel),
-    leftTrigger.addEventListener("mouseout", toggleLeftPanel),
-    rightTrigger.addEventListener("mouseover", toggleRightPanel),
-    rightTrigger.addEventListener("mouseout", toggleRightPanel),
-    setTimeout(function () {
-        loadDeferContent();
-    }, 1500);
+circle1.addEventListener("click", () => scrollMain(false));
+circle2.addEventListener("click", () => scrollMain(true));
+
+main.addEventListener("wheel", (event) => {
+    setColor(event.deltaY > 0);
+}, { passive: true });
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowDown" || event.key === "ArrowRight") {
+        scrollMain(true);
+    } else if (event.key === "ArrowUp" || event.key === "ArrowLeft") {
+        scrollMain(false);
+    }
+});
+
+maoyeedy.addEventListener("mouseover", toggleLeftPanel);
+maoyeedy.addEventListener("mouseout", toggleLeftPanel);
+redButton.addEventListener("mouseover", toggleRightPanel);
+redButton.addEventListener("mouseout", toggleRightPanel);
+leftPanel.addEventListener("mouseover", toggleLeftPanel);
+leftPanel.addEventListener("mouseout", toggleLeftPanel);
+rightPanel.addEventListener("mouseover", toggleRightPanel);
+rightPanel.addEventListener("mouseout", toggleRightPanel);
+leftTrigger.addEventListener("mouseover", toggleLeftPanel);
+leftTrigger.addEventListener("mouseout", toggleLeftPanel);
+rightTrigger.addEventListener("mouseover", toggleRightPanel);
+rightTrigger.addEventListener("mouseout", toggleRightPanel);
+
+setTimeout(loadDeferContent, 1500);
