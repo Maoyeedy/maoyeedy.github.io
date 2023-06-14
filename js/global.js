2b2a4d7f68c6a291201314bf9147d1af78c6927b
Maoyeedy <100255436+Maoyeedy@users.noreply.github.com>
2023-06-14 13:19:26 +0800

    0.0

--- js/global.js
+++ js/global.js
@@ -1,11 +1,13 @@
-const img = document.querySelectorAll('img')
-img.forEach((image) => {
-  image.draggable = false
-})
+document.addEventListener('DOMContentLoaded', function () {
+  const img = document.querySelectorAll('img')
+  img.forEach((image) => {
+    image.draggable = false
+  })
 
-const anchors = document.querySelectorAll('a')
-anchors.forEach((anchor) => {
-  // anchor.target = '_blank';
-  anchor.rel = 'noreferrer noopener'
-  anchor.draggable = false
+  const anchors = document.querySelectorAll('a')
+  anchors.forEach((anchor) => {
+    // anchor.target = '_blank';
+    anchor.rel = 'noreferrer noopener'
+    anchor.draggable = false
+  })
 })
