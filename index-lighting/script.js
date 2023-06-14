445cc8b507823bba23695c08cd76475948ca0b1f
Maoyeedy <100255436+Maoyeedy@users.noreply.github.com>
2023-06-15 00:07:07 +0800

    0.0

--- index-lighting/script.js
+++ index-lighting/script.js
@@ -3,15 +3,12 @@ const observer = new IntersectionObserver((entries) => {
     console.log(entry)
     if (entry.isIntersecting) {
       entry.target.classList.add('show')
-      // if (entry.target === endOfScroll) {
-      //     document.documentElement.style.scrollSnapType = 'none'
-      // }
-    } else {
-      entry.target.classList.remove('show')
     }
+    // else {
+    //   entry.target.classList.remove('show')
+    // }
   })
 })
 
-// const endOfScroll = document.getElementById('endofscroll')
 const pages = document.querySelectorAll('.page')
 pages.forEach((el) => observer.observe(el))
---
+++ index-modelviewer/script.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show')
+    }
+    else {
+      entry.target.classList.remove('show')
+    }
+  })
+})
+
+const cards = document.querySelectorAll('.card')
+cards.forEach((el) => observer.observe(el))
