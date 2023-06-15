be28883c11da599970a61aa06b1b8560d841b542
Maoyeedy <100255436+Maoyeedy@users.noreply.github.com>
2023-06-15 18:51:34 +0800

    0.0

--- index-qiyu/1.cs
+++ index-qiyu/1.cs
@@ -1,71 +1,112 @@
-﻿using System;
-using System.Collections;
-using TMPro;
 using UnityEngine;
+using DG.Tweening;
 
-public class DialogPlayer : MonoBehaviour
+public class CameraZoom : MonoBehaviour
 {
-    public Dialog[] dialogs;
-    public TextMeshProUGUI dialogText, nameText;
-    public float characterInterval = 0.05f, autoNextLineTime = 2.5f;
-    private float _elapsedTime;
-    private int _index;
+    [Header("FOV")] 
+    public float fovRange = 8f;
+    public float fovDelta = 4f;
+
+    [Header("Camera Distance")] 
+    public float distanceRange = 0.25f;
+    public float distanceDelta = 0.2f;
+
+    [Header("Factors")] 
+    public float zoomDuration = 0.3f;
+    public float zoomInFactor = 1.5f;
+    public float rightOffsetFactor = 0.3f;
+    public float scrollFactor = 0.025f;
+
+    private vThirdPersonCamera cam;
+    private float FOV, FOVmin, FOVmax, FOVdefault, tempFOV, tempDistance, defaultDistance, defaultRightOffset;
 
     private void Start()
     {
-        dialogText.text = string.Empty;
-        nameText.text = string.Empty;
-        StartCoroutine(TypeLine());
+        cam = GetComponent<vThirdPersonCamera>();
+        defaultDistance = cam.defaultDistance;
+        tempDistance = defaultDistance;
+        defaultRightOffset = cam.rightOffset;
+
+        FOV = GetComponent<Camera>().fieldOfView;
+        FOVdefault = FOV;
+        tempFOV = FOV;
+        SetMinMax();
     }
 
     private void Update()
     {
-        _elapsedTime += Time.deltaTime;
-        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Return) || _elapsedTime > autoNextLineTime) SwitchLine();
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+                ZoomOut();
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+            ZoomToDefault();
+
+        if (Input.GetMouseButtonDown(1))
+            ZoomIn();
+
+        if (Input.GetMouseButtonUp(1))
+            ZoomToDefault();
+
+        if (Input.mouseScrollDelta.y != 0)
+            ZoomOnMouseScroll();
     }
 
-    private void SwitchLine()
+    private void ZoomIn()
     {
-         _elapsedTime = 0;
-        if (dialogText.text == dialogs[_index].dialog)
-        {
-            NextLine();
-        }
-        else
-        {
-            StopAllCoroutines();
-            dialogText.text = dialogs[_index].dialog;
-        }
+        tempFOV = FOV;
+        FOVmin -= fovDelta * 2f;
+
+        DOTween.To(() => FOV, x => FOV = x, FOV - fovDelta * 2f, zoomDuration * zoomInFactor)
+            .OnUpdate(() => GetComponent<Camera>().fieldOfView = FOV);
+
+        DOTween.To(() => tempDistance, x => tempDistance = x, tempDistance - distanceDelta * 1.75f, zoomDuration * zoomInFactor)
+            .OnUpdate(() => cam.defaultDistance = tempDistance);
+
+        DOTween.To(() => defaultRightOffset, x => defaultRightOffset = x, defaultRightOffset + distanceDelta * rightOffsetFactor, zoomDuration * zoomInFactor)
+            .OnUpdate(() => cam.rightOffset = defaultRightOffset);
     }
 
-    private void NextLine()
+    private void ZoomOut()
     {
-        dialogText.text = string.Empty;
-        if (_index < dialogs.Length - 1)
-        {
-            _index++;
-            StartCoroutine(TypeLine());
-            nameText.text = dialogs[_index].name;
-        }
-        else
-        {
-            gameObject.SetActive(false);
-        }
+        tempFOV = FOV;
+        FOVmax += fovDelta;
+
+        DOTween.To(() => FOV, x => FOV = x, FOV + fovDelta, zoomDuration)
+            .OnUpdate(() => GetComponent<Camera>().fieldOfView = FOV);
+
+        DOTween.To(() => tempDistance, x => tempDistance = x, tempDistance - distanceDelta, zoomDuration)
+            .OnUpdate(() => cam.defaultDistance = tempDistance);
     }
 
-    private IEnumerator TypeLine()
+    private void ZoomToDefault()
     {
-        foreach (char x in dialogs[_index].dialog)
-        {
-            dialogText.text += x;
-            yield return new WaitForSeconds(characterInterval);
-        }
+        DOTween.To(() => FOV, x => FOV = x, tempFOV, zoomDuration)
+            .OnUpdate(() => GetComponent<Camera>().fieldOfView = FOV);
+
+        DOTween.To(() => cam.defaultDistance, x => cam.defaultDistance = x, tempDistance, zoomDuration)
+            .OnUpdate(() => cam.defaultDistance = tempDistance);
+
+        DOTween.To(() => cam.rightOffset, x => cam.rightOffset = x, defaultRightOffset, zoomDuration).OnUpdate(() => cam.rightOffset = defaultRightOffset);
+        SetMinMax();
     }
 
-    [Serializable]
-    public struct Dialog
+    private void ZoomOnMouseScroll()
     {
-        public string name;
-        public string dialog;
+        float scrollDelta = Input.mouseScrollDelta.y;
+
+        tempDistance -= scrollDelta * scrollFactor;
+        tempDistance = Mathf.Clamp(tempDistance, defaultDistance - distanceRange, defaultDistance + distanceRange);
+        cam.defaultDistance = tempDistance;
+
+        FOV -= scrollDelta * scrollFactor * 20;
+        FOV = Mathf.Clamp(FOV, FOVmin, FOVmax);
+        GetComponent<Camera>().fieldOfView = FOV;
+    }
+
+    private void SetMinMax()
+    {
+        FOVmax = FOVdefault + fovRange;
+        FOVmin = FOVdefault - fovRange * 0.75f;
     }
 }
--- index-qiyu/2.cs
+++ index-qiyu/2.cs
@@ -1,112 +1,71 @@
+﻿using System;
+using System.Collections;
+using TMPro;
 using UnityEngine;
-using DG.Tweening;
 
-public class CameraZoom : MonoBehaviour
+public class DialogPlayer : MonoBehaviour
 {
-    [Header("FOV")] 
-    public float fovRange = 8f;
-    public float fovDelta = 4f;
-
-    [Header("Camera Distance")] 
-    public float distanceRange = 0.25f;
-    public float distanceDelta = 0.2f;
-
-    [Header("Factors")] 
-    public float zoomDuration = 0.3f;
-    public float zoomInFactor = 1.5f;
-    public float rightOffsetFactor = 0.3f;
-    public float scrollFactor = 0.025f;
-
-    private vThirdPersonCamera cam;
-    private float FOV, FOVmin, FOVmax, FOVdefault, tempFOV, tempDistance, defaultDistance, defaultRightOffset;
+    public Dialog[] dialogs;
+    public TextMeshProUGUI dialogText, nameText;
+    public float characterInterval = 0.05f, autoNextLineTime = 2.5f;
+    private float _elapsedTime;
+    private int _index;
 
     private void Start()
     {
-        cam = GetComponent<vThirdPersonCamera>();
-        defaultDistance = cam.defaultDistance;
-        tempDistance = defaultDistance;
-        defaultRightOffset = cam.rightOffset;
-
-        FOV = GetComponent<Camera>().fieldOfView;
-        FOVdefault = FOV;
-        tempFOV = FOV;
-        SetMinMax();
+        dialogText.text = string.Empty;
+        nameText.text = string.Empty;
+        StartCoroutine(TypeLine());
     }
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
-            if (Input.GetKeyDown(KeyCode.LeftShift))
-                ZoomOut();
-
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-            ZoomToDefault();
-
-        if (Input.GetMouseButtonDown(1))
-            ZoomIn();
-
-        if (Input.GetMouseButtonUp(1))
-            ZoomToDefault();
-
-        if (Input.mouseScrollDelta.y != 0)
-            ZoomOnMouseScroll();
+        _elapsedTime += Time.deltaTime;
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Return) || _elapsedTime > autoNextLineTime) SwitchLine();
     }
 
-    private void ZoomIn()
+    private void SwitchLine()
     {
-        tempFOV = FOV;
-        FOVmin -= fovDelta * 2f;
-
-        DOTween.To(() => FOV, x => FOV = x, FOV - fovDelta * 2f, zoomDuration * zoomInFactor)
-            .OnUpdate(() => GetComponent<Camera>().fieldOfView = FOV);
-
-        DOTween.To(() => tempDistance, x => tempDistance = x, tempDistance - distanceDelta * 1.75f, zoomDuration * zoomInFactor)
-            .OnUpdate(() => cam.defaultDistance = tempDistance);
-
-        DOTween.To(() => defaultRightOffset, x => defaultRightOffset = x, defaultRightOffset + distanceDelta * rightOffsetFactor, zoomDuration * zoomInFactor)
-            .OnUpdate(() => cam.rightOffset = defaultRightOffset);
+         _elapsedTime = 0;
+        if (dialogText.text == dialogs[_index].dialog)
+        {
+            NextLine();
+        }
+        else
+        {
+            StopAllCoroutines();
+            dialogText.text = dialogs[_index].dialog;
+        }
     }
 
-    private void ZoomOut()
+    private void NextLine()
     {
-        tempFOV = FOV;
-        FOVmax += fovDelta;
-
-        DOTween.To(() => FOV, x => FOV = x, FOV + fovDelta, zoomDuration)
-            .OnUpdate(() => GetComponent<Camera>().fieldOfView = FOV);
-
-        DOTween.To(() => tempDistance, x => tempDistance = x, tempDistance - distanceDelta, zoomDuration)
-            .OnUpdate(() => cam.defaultDistance = tempDistance);
+        dialogText.text = string.Empty;
+        if (_index < dialogs.Length - 1)
+        {
+            _index++;
+            StartCoroutine(TypeLine());
+            nameText.text = dialogs[_index].name;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
     }
 
-    private void ZoomToDefault()
+    private IEnumerator TypeLine()
     {
-        DOTween.To(() => FOV, x => FOV = x, tempFOV, zoomDuration)
-            .OnUpdate(() => GetComponent<Camera>().fieldOfView = FOV);
-
-        DOTween.To(() => cam.defaultDistance, x => cam.defaultDistance = x, tempDistance, zoomDuration)
-            .OnUpdate(() => cam.defaultDistance = tempDistance);
-
-        DOTween.To(() => cam.rightOffset, x => cam.rightOffset = x, defaultRightOffset, zoomDuration).OnUpdate(() => cam.rightOffset = defaultRightOffset);
-        SetMinMax();
+        foreach (char x in dialogs[_index].dialog)
+        {
+            dialogText.text += x;
+            yield return new WaitForSeconds(characterInterval);
+        }
     }
 
-    private void ZoomOnMouseScroll()
+    [Serializable]
+    public struct Dialog
     {
-        float scrollDelta = Input.mouseScrollDelta.y;
-
-        tempDistance -= scrollDelta * scrollFactor;
-        tempDistance = Mathf.Clamp(tempDistance, defaultDistance - distanceRange, defaultDistance + distanceRange);
-        cam.defaultDistance = tempDistance;
-
-        FOV -= scrollDelta * scrollFactor * 20;
-        FOV = Mathf.Clamp(FOV, FOVmin, FOVmax);
-        GetComponent<Camera>().fieldOfView = FOV;
-    }
-
-    private void SetMinMax()
-    {
-        FOVmax = FOVdefault + fovRange;
-        FOVmin = FOVdefault - fovRange * 0.75f;
+        public string name;
+        public string dialog;
     }
 }
