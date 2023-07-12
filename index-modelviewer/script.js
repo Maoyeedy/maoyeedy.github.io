014da7a5b02d6c520e11bc6f1d3dccdc874cc825
Maoyeedy <100255436+Maoyeedy@users.noreply.github.com>
2023-07-12 23:20:57 +0800

    update pkgs

--- index-modelviewer/script.js
+++ index-modelviewer/script.js
@@ -2,8 +2,7 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add('show')
-    }
-    else {
+    } else {
       entry.target.classList.remove('show')
     }
   })
